fda7a73394e1fa68b3202165ace52f7ef4b86812
Adhithya <its.g.adhithya@gmail.com>
2026-05-05 14:03:42 +0530

    Finishing up with Dijkstra

---
+++ dijkstra.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+def dijkstra(graph, start):
+    dist = {node: float("inf") for node in graph}
+    dist[start] = 0
+    pq = [(0, start)]
+
+    while pq:
+        d, node = heapq.heappop(pq)
+
+        if d > dist[node]:
+            continue
+
+        for nei, w in graph[node]:
+            if dist[nei] > d + w:
+                dist[nei] = d + w
+                heapq.heappush(pq, (dist[nei], nei))
+
+    return dist
